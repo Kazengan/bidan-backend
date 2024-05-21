f260424185cfd6a895430c39b231c53a2a56c244
Kazengan <32446394+Kazengan@users.noreply.github.com>
2024-05-21 13:56:11 +0000

    add function edit

---
+++ edit/edit.go
@@ -0,0 +1,17 @@
+package edit
+
+import import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+function Edit(w http.ResponseWriter, r *http.Request) {
+}
