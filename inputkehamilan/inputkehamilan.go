d36cf8acaa608a12c83bcd861aee3f31a58b3552
zzail <zzaidanluthfi@gmail.com>
2024-05-24 19:58:12 +0000

    minor changes

--- inputkehamilan/inputkehamilan.go
+++ inputkehamilan/inputkehamilan.go
@@ -17,16 +17,16 @@ func getNextPasien(db *mongo.Database) (uint64, error) {
 	collection := db.Collection("pasien_counter")
 	filter := bson.M{}
 	update := bson.M{"$inc": bson.M{"seq_value": 1}}
-	options := options.FindOneAndUpdate().SetReturnDocument(options.After)
+	opts := options.FindOneAndUpdate().SetReturnDocument(options.After)
 	var updatedCounter bson.M
-	err := collection.FindOneAndUpdate(context.Background(), filter, update, options).Decode(&updatedCounter)
+	err := collection.FindOneAndUpdate(context.Background(), filter, update, opts).Decode(&updatedCounter)
 	if err != nil {
 		return 0, err
 	}
 
 	sequenceValue, ok := updatedCounter["seq_value"].(int64)
 	if !ok {
-		return 0, fmt.Errorf("sequence_value is not of type int64, sequence_value: %v, sequence_value type: %T", updatedCounter["sequence_value"], updatedCounter["sequence_value"])
+		return 0, fmt.Errorf("sequence_value is not of type int64, sequence_value: %v, sequence_value type: %T", updatedCounter["seq_value"], updatedCounter["seq_value"])
 	}
 
 	return uint64(sequenceValue), nil
@@ -34,57 +34,46 @@ func getNextPasien(db *mongo.Database) (uint64, error) {
 
 func InputKehamilan(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	err := godotenv.Load()
-	if err != nil {
-		jsonData, _ := json.Marshal(map[string]interface{}{"message": ".env not found"})
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write(jsonData)
+
+	if err := godotenv.Load(); err != nil {
+		http.Error(w, `{"message": ".env not found"}`, http.StatusInternalServerError)
+		return
 	}
 
 	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
 	if err != nil {
-		jsonData, _ := json.Marshal(map[string]interface{}{"message": "error connecting to database"})
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write(jsonData)
+		http.Error(w, `{"message": "error connecting to database"}`, http.StatusInternalServerError)
+		return
 	}
-
 	defer func() {
 		if err := client.Disconnect(context.Background()); err != nil {
-			jsonData, _ := json.Marshal(map[string]interface{}{"message": "Database disconnected"})
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write(jsonData)
+			http.Error(w, `{"message": "error disconnecting from database"}`, http.StatusInternalServerError)
 		}
 	}()
+
 	db := client.Database("mydb")
 	collection := db.Collection("pasien")
 
 	var dataMap map[string]interface{}
-	decoder := json.NewDecoder(r.Body)
-	if err := decoder.Decode(&dataMap); err != nil {
-		jsonData, _ := json.Marshal(map[string]interface{}{"message": "error decoding data from request body"})
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write(jsonData)
+	if err := json.NewDecoder(r.Body).Decode(&dataMap); err != nil {
+		http.Error(w, `{"message": "error decoding data from request body"}`, http.StatusBadRequest)
 		return
 	}
 
-	data := dataMap["data"].(map[string]interface{})
-	if data == nil {
-		jsonData, _ := json.Marshal(map[string]interface{}{"message": "data needed"})
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write(jsonData)
+	data, ok := dataMap["data"].(map[string]interface{})
+	if !ok {
+		http.Error(w, `{"message": "data field is required"}`, http.StatusBadRequest)
 		return
 	}
 
-	next_id_pasien, err := getNextPasien(db)
+	nextIDPasien, err := getNextPasien(db)
 	if err != nil {
-		w.Header().Set("Content-Type", "text/plain")
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error() + "\naaaaaaaa"))
+		http.Error(w, fmt.Sprintf(`{"message": "error getting next pasien id: %v"}`, err), http.StatusInternalServerError)
 		return
 	}
 
 	dataPasien := bson.M{
-		"id_pasien":          next_id_pasien,
+		"id_pasien":          nextIDPasien,
 		"tanggal_register":   data["generalInformation"].(map[string]interface{})["tanggalRegister"],
 		"nama_pasien":        data["generalInformation"].(map[string]interface{})["namaLengkap"],
 		"tanggal_lahir":      data["generalInformation"].(map[string]interface{})["tanggalLahir"],
@@ -112,19 +101,18 @@ func InputKehamilan(w http.ResponseWriter, r *http.Request) {
 			"persalinan":                  data["persalinan"],
 			"rencana_persalinan":          data["rencanaPersalinan"],
 			"riwayat_kehamilan":           data["riwayatKehamilan"],
+			"screeningTT":                 data["screeningTT"],
 			"section2":                    data["section2"],
 		},
 	}
 
 	if _, err := collection.InsertOne(context.Background(), dataPasien); err != nil {
-		jsonData, _ := json.Marshal(map[string]interface{}{"message": "error inserting data"})
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write(jsonData)
+		http.Error(w, `{"message": "error inserting data"}`, http.StatusInternalServerError)
 		return
 	}
 
-	jsonData, _ := json.Marshal(map[string]interface{}{"message": "success", "id_pasien": next_id_pasien})
+	response := map[string]interface{}{"message": "success", "id_pasien": nextIDPasien}
+	jsonData, _ := json.Marshal(response)
 	w.WriteHeader(http.StatusOK)
 	w.Write(jsonData)
-
 }
