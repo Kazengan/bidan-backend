28fce35358343510cdb065cf8634385a5637a29b
projectbidanmandiri <projectbidanmandiri@gmail.com>
2024-07-23 16:40:05 +0000

    bug fixes

--- main.go
+++ main.go
@@ -5,17 +5,21 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/Kazengan/bidan-backend/allsoap"
 	"github.com/Kazengan/bidan-backend/bidanlogin"
 	"github.com/Kazengan/bidan-backend/chart"
+	"github.com/Kazengan/bidan-backend/chartt"
 	"github.com/Kazengan/bidan-backend/count"
+	"github.com/Kazengan/bidan-backend/countanually"
 	"github.com/Kazengan/bidan-backend/countt"
+	"github.com/Kazengan/bidan-backend/delete"
 	"github.com/Kazengan/bidan-backend/deletebidan"
 	"github.com/Kazengan/bidan-backend/edit"
 	"github.com/Kazengan/bidan-backend/editimunisasi"
 	"github.com/Kazengan/bidan-backend/editkb"
 	"github.com/Kazengan/bidan-backend/export"
 	"github.com/Kazengan/bidan-backend/findpasien"
-	getallbidan "github.com/Kazengan/bidan-backend/getbidan"
+	"github.com/Kazengan/bidan-backend/getbidan"
 	"github.com/Kazengan/bidan-backend/getpasien"
 	"github.com/Kazengan/bidan-backend/getreservasi"
 	"github.com/Kazengan/bidan-backend/helper"
@@ -40,12 +44,16 @@ func main() {
 	if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
 		listenAddr = ":" + val
 	}
+	http.HandleFunc("/api/allsoap", allsoap.Allsoap)
 	http.HandleFunc("/api/bidanlogin", bidanlogin.LoginHandler)
 	http.HandleFunc("/api/getpasien", getpasien.GetPasien)
 	http.HandleFunc("/api/getreservasi", getreservasi.GetReservasi)
 	http.HandleFunc("/api/count", count.CountHandler)
+	http.HandleFunc("/api/countanually", countanually.CountHandler)
 	http.HandleFunc("/api/countt", countt.CountHandler)
 	http.HandleFunc("/api/chart", chart.Chart)
+	http.HandleFunc("/api/chartt", chartt.Chartt)
+	http.HandleFunc("/api/delete", delete.Delete)
 	http.HandleFunc("/api/editkb", editkb.EditKb)
 	http.HandleFunc("/api/editimunisasi", editimunisasi.EditImunisasi)
 	http.HandleFunc("/api/edit", edit.Edit)
@@ -53,6 +61,7 @@ func main() {
 	http.HandleFunc("/api/inputkb", inputkb.InputKB)
 	http.HandleFunc("/api/input", input.Input)
 	http.HandleFunc("/api/soap", soap.Soap)
+
 	http.HandleFunc("/api/soapkb", soapkb.SoapKB)
 	http.HandleFunc("/api/soapimunisasi", soapimunisasi.SoapImunisasi)
 	http.HandleFunc("/api/soapkehamilan", soapkehamilan.SoapKehamilan)
