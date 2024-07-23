8f0584063a3ac53bc040f5b15559111cea904988
projectbidanmandiri <projectbidanmandiri@gmail.com>
2024-07-23 16:34:57 +0000

    minor changes

--- count/count.go
+++ count/count.go
@@ -3,6 +3,7 @@ package count
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"os"
 	"strconv"
@@ -14,38 +15,67 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func CountHandler(w http.ResponseWriter, r *http.Request) {
+func connectToDatabase() (*mongo.Client, error) {
+	err := godotenv.Load()
+	if err != nil {
+		return nil, fmt.Errorf(".env not found")
+	}
+
+	clientOptions := options.Client().ApplyURI(os.Getenv("MONGODB_URI"))
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
+}
+
+func respondWithError(w http.ResponseWriter, status int, message string) {
 	w.Header().Set("Content-Type", "application/json")
-	idLayanan, err := strconv.Atoi(r.URL.Query().Get("id_layanan"))
+	jsonData, _ := json.Marshal(map[string]string{"message": message})
+	w.WriteHeader(status)
+	w.Write(jsonData)
+}
 
+func respondWithJSON(w http.ResponseWriter, status int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	jsonData, _ := json.Marshal(payload)
+	w.WriteHeader(status)
+	w.Write(jsonData)
+}
+
+func CountHandler(w http.ResponseWriter, r *http.Request) {
+	client, err := connectToDatabase()
 	if err != nil {
-		somethingWentWrong, _ := json.Marshal(map[string]interface{}{"message": "Invalid id_layanan", "statusCode": 400})
-		w.Write(somethingWentWrong)
-		return
-	}
-
-	if err := godotenv.Load(); err != nil {
-		somethingWentWrong, _ := json.Marshal(map[string]interface{}{"message": "Failed to load .env file", "statusCode": 400})
-		w.Write(somethingWentWrong)
-		return
-	}
-
-	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
-	if err != nil {
-		somethingWentWrong, _ := json.Marshal(map[string]interface{}{"message": "Failed to connect to database", "statusCode": 400})
-		w.Write(somethingWentWrong)
+		respondWithError(w, http.StatusInternalServerError, "Error connecting to database")
 		return
 	}
 
 	defer func() {
-		if err := client.Disconnect(context.Background()); err != nil {
-			somethingWentWrong, _ := json.Marshal(map[string]interface{}{"message": "Database disconnected", "statusCode": 400})
-			w.Write(somethingWentWrong)
+		if err := client.Disconnect(context.TODO()); err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Database disconnect error")
 			return
 		}
 	}()
 
 	db := client.Database("mydb")
+
+	// Retrieve optional id_layanan parameter
+	idLayananStr := r.URL.Query().Get("id_layanan")
+	var idLayanan int
+	if idLayananStr != "" {
+		idLayanan, err = strconv.Atoi(idLayananStr)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Invalid id_layanan")
+			return
+		}
+	} else {
+		respondWithJSON(w, http.StatusOK, map[string]interface{}{"statusCode": 200, "message": "Success", "jumlah": 0, "lastUpdate": nil})
+		return
+	}
+
+	currentYear := time.Now().Year()
+	currentMonth := int(time.Now().Month())
+	strTanggal := fmt.Sprintf("%04d-%02d", currentYear, currentMonth)
 
 	var collectionName string
 	var dateField string
@@ -61,50 +91,37 @@ func CountHandler(w http.ResponseWriter, r *http.Request) {
 		collectionName = "soap_imunisasi"
 		dateField = "tglDatang"
 	default:
-		jsonData, _ := json.Marshal(map[string]interface{}{"statusCode": 200, "message": "Success", "jumlah": 0, "lastUpdate": nil})
-		w.Write(jsonData)
+		respondWithJSON(w, http.StatusOK, map[string]interface{}{"statusCode": 200, "message": "Success", "jumlah": 0, "lastUpdate": nil})
 		return
 	}
 
 	collection := db.Collection(collectionName)
-	now := time.Now().UTC()
-	startOfMonth := time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, time.UTC)
-	endOfMonth := startOfMonth.AddDate(0, 1, 0)
-
-	filterCriteria := bson.M{dateField: bson.M{"$gte": startOfMonth.Format(time.RFC3339), "$lt": endOfMonth.Format(time.RFC3339)}}
+	filterCriteria := bson.M{dateField: bson.M{"$regex": "^" + strTanggal}}
 	cursor, err := collection.Find(context.Background(), filterCriteria)
 	if err != nil {
-		somethingWentWrong, _ := json.Marshal(map[string]interface{}{"message": "Finding documents went wrong", "statusCode": 400})
-		w.Write(somethingWentWrong)
+		respondWithError(w, http.StatusInternalServerError, "Error finding documents")
 		return
 	}
 	defer cursor.Close(context.Background())
 
-	var lastUpdate time.Time
+	var lastUpdate string
 	var countData int64
 	for cursor.Next(context.Background()) {
 		countData++
 		var doc bson.M
 		if err := cursor.Decode(&doc); err != nil {
-			somethingWentWrong, _ := json.Marshal(map[string]interface{}{"message": "Decoding document went wrong", "statusCode": 400})
-			w.Write(somethingWentWrong)
+			respondWithError(w, http.StatusInternalServerError, "Error decoding document")
 			return
 		}
 
-		var docDate time.Time
 		if date, ok := doc[dateField].(string); ok {
-			docDate, err = time.Parse(time.RFC3339, date)
-			if err != nil {
-				somethingWentWrong, _ := json.Marshal(map[string]interface{}{"message": "Parsing date went wrong", "statusCode": 400})
-				w.Write(somethingWentWrong)
-				return
-			}
-		}
-		if docDate.After(lastUpdate) {
-			lastUpdate = docDate
+			lastUpdate = date
 		}
 	}
 
-	jsonData, _ := json.Marshal(map[string]interface{}{"statusCode": 200, "message": "Success", "jumlah": countData, "lastUpdate": lastUpdate.Format(time.RFC3339)})
-	w.Write(jsonData)
+	if countData == 0 {
+		lastUpdate = fmt.Sprintf("%s-01T00:00:00Z", strTanggal)
+	}
+
+	respondWithJSON(w, http.StatusOK, map[string]interface{}{"statusCode": 200, "message": "Success", "jumlah": countData, "lastUpdate": lastUpdate, "strTanggal": strTanggal})
 }
